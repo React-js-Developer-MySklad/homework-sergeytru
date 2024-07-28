0d1df3bbb68950f7b4c922fd8571e3ca79e260df
strubitsyn <strubitsyn@moysklad.ru>
2024-07-28 20:07:10 +0300

    hw2 table, first attempt

--- src/app/app.js
+++ src/app/app.js
@@ -1,6 +1,62 @@
 import html from "./app.html";
 import './app.css'
+import item_template from "./item.html";
 
+const counterparties = [
+    {
+        "id": -5,
+        "Наименование": "Московская биржа ММВБ-РТС",
+        "ИНН": "7702077840",
+        "Адрес": "Москва, ул. Фруктовая, 11А",
+        "КПП": "1027739387411"
+    },
+    {
+        "id": -4,
+        "Наименование": "СПБ Биржа",
+        "ИНН": "7801268965",
+        "Адрес": "Санкт-Петербург, Речная ул. 5",
+        "КПП": "1097800000440"
+    },
+    {
+        "id": -3,
+        "Наименование": "Санкт-Петербургская валютная биржа",
+        "ИНН": "7825331045",
+        "Адрес": "Санкт-Петербург, Васи Алексеева ул., 14",
+        "КПП": "1037843013812"
+    },
+    {
+        "id": -2,
+        "Наименование": "Санкт-Петербургская Международная Товарно-сырьевая Биржа",
+        "ИНН": "7840389730",
+        "Адрес": "Санкт-Петербург, Нейшлотский пер. 7 лит Б корп 5",
+        "КПП": "108984718890"
+    },
+    {
+        "id": -1,
+        "Наименование": "Национальный Клиринговый Центр",
+        "ИНН": "7750004023",
+        "Адрес": "Москва, 2-я Дзержинская ул., 17",
+        "КПП": "1067711004481"
+    }
+];
+
+const buildByTemplate = function (data) {
+    let result = item_template;
+    for (const column of ["Наименование", "ИНН", "Адрес", "КПП"]) {
+        result = result.replaceAll("$" + column + "$", data[column])
+    }
+    return result;
+}
 
 const rootElement = document.getElementById('root');
 rootElement.innerHTML = html;
+
+const tableBody = rootElement.querySelector(".main-data");
+
+function updateTableBody() {
+    tableBody.innerHTML = counterparties.map(oneof => buildByTemplate(oneof)).join("\n");
+    //todo: редактирование по двойному нажатию
+    //todo: удаление строки по кнопке
+}
+
+updateTableBody();
